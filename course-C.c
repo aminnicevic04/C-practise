dfb84a3024a0bc7ed0bbc38f880b29e845e8c215
aminnicevic04 <aminnicevic@gmail.com>
2023-11-08 13:42:36 +0100

    update

---
+++ course-C.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <math.h>
+
+// typedef int bool; // Define bool as an alias for int
+// #define true 1    // Define true as 1
+// #define false 0   // Define false as 0
+
+int main() {
+
+    // int x; //declaration
+    // x = 23; //initialization
+    // int y = 32; //declaration + initialization
+
+    // int age = 21; //integer
+    // float gpa = 2.05; //floating point number
+    // char grade = 'C'; //single character
+    // char name[] = "Bro"; //array of characters
+    // bool isTrue = false;
+    // char f = 100;
+    
+    // printf("Hello %s\n", name);
+    // printf("You are age %d years old\n", age);
+    // printf("Your average grade is %c\n", grade);
+    // printf("Your gpa is %f\n", gpa);
+    // printf("%d\n", isTrue);
+
+    //2
+
+    //format specifier % = define formats a type of data to be displayed
+
+    // %c = character
+    // %s = string
+    // %f = float
+    // %lf = double
+    // %d = intager
+
+    // %.1 = decimal precision
+    // %1 = minimum field width
+    // %- = left align 
+
+    // float item1 = 5.76;
+    // float item2 = 10.00;
+    // float item3 = 100.99900;
+    // float item4 = 1.99;
+    // float item5 = 45.60;
+
+    // printf("Item 1: $%.2f \n", item1);
+    // printf("Item 2: $%8.2f \n", item2);
+    // printf("Item 3: $%-8.2f \n", item3);
+    // printf("Item 4: $%.f \n", item4);
+    // printf("Item 5: $%f \n", item5); 
+
+    //3 constant - fixed value
+
+
+    // const float PI = 3.14159;
+    
+    // printf("%f", PI);
+
+    //4 arithemtic operators
+
+    // + (adition)
+    // - (subtraction)
+    // * (multiplicaton)
+    // / (division)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)
+
+    //5 augment assignment opperators 
+
+    // int x = 10;
+    // x = x+2;
+    // x+=2;
+
+    // 6 user input
+
+    // int age; 
+    // char name[25];
+
+    // printf("whats ur name\namin");
+    // scanf("%s", &name);
+
+    // printf("how old are u?\n");
+    // scanf("%d", &age);
+
+    // printf("hello %s, how are u?\n",name);
+    // printf("u are %d years old", age);
+
+    // 7 math 
+
+    // double A = sqrt(9); //koren 3
+    // double B = pow(2, 4); //kvadrat 16
+    // int C = round(3.14); //zaokruzivanje 3
+    // int D = ceil(3.14); //zaokruzivanje na vise. 4
+    // int E = floor(3.99); //zaokruzivanje na manje. 3
+    // double F = fabs(-100); //absolutna vrednost 100.000000
+    // double G = log(3); //logaritam 1.098612
+    // double H = sin(45); //sinus 0.850904
+    // double I = cos(45); //kosinus 0.525322
+    // double J = tan(45); //tangens 1.619775
+
+    // printf("\n%d", C); //for int
+    // printf("\n%lf", H); // for double
+
+
+
+
+    return 0;
+}
