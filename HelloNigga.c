1128c36c0ea1e5862c824e44eb551b917bf31ef2
aminnicevic04 <aminnicevic@gmail.com>
2023-11-08 13:44:19 +0100

    update

---
+++ HelloNigga.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    
+    printf("nigga number : ");       
+    scanf("%d", &a);
+    printf("Nigga number : %d is the darkest nigga in city",a);
+    
+    return 0;
+}
