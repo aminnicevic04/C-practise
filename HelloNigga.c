96db2a49d9b9a22e68f46de6ab1158f800cf170b
Amin Nicevic <114291827+aminnicevic04@users.noreply.github.com>
2024-04-21 22:12:03 +0200

    Update HelloNigga.c

--- HelloNigga.c
+++ HelloNigga.c
@@ -5,7 +5,7 @@ int main(){
     
     printf("nigga number : ");       
     scanf("%d", &a);
-    printf("Nigga number : %d is the darkest nigga in city",a);
+    printf("Nigga number : %d is the darkest nigga in city.",a);
     
     return 0;
 }
